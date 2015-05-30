9187920bad75a5ecf261b01a47337d215aeb862b
Rob Schluntz <saitou1024@gmail.com>
2015-05-29 21:40:48 -0400

    sample test suite

---
+++ src/test/scala/game/GameMapSuite.scala
@@ -0,0 +1,19 @@
+package ca.uwaterloo.cs.rschlunt
+package tdtd
+package game
+
+import org.scalatest.FunSuite
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class RegExSuite extends FunSuite {
+  test("test map") {
+    assert(4 === 5)
+  }
+
+  test("test insert") {
+    
+  }
+}
